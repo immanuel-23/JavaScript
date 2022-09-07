cfd3c58f920efd65cc07fd70646ed370974c8a98
Immanuel <immanuelgk@gmail.com>
2022-09-07 11:30:16 +0530

    ajax

---
+++ ajax.js
@@ -0,0 +1,13 @@
+console.log(1);
+console.log(2);
+console.log(3);
+setTimeout(() => {
+    console.log("waiting for api");
+    // set time out is ajax it will print or wait till end in this example
+});
+for (let i = 0; i < 500; i++) {
+console.log(i);    
+}
+console.log(4);
+console.log(5);
+console.log(6);
